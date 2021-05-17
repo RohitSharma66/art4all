fe03b6f607103c041eae2ee3bab80b074006dc6e
Rohit Sharma <rohitsharma@MacBook-Pro-2.broadband>
2021-05-17 18:44:45 +0100

    arts

--- app/models/art.rb
+++ app/models/art.rb
@@ -2,7 +2,7 @@ class Art < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_one_attached :photo
-  validates :name, :artist, :price, :style, presence: true
+  validates :name, :artist, :price, :style, :rate, :photo, presence: true
 
   STYLES = [ 'abstract', 'fine art', 'modern', 'figurative', 'abstract expressionism', 'expressionism', 'realism', 'conceptual impressionism', 'surrealism', 'portraiture', 'pop art', 'minimalism', 'illustration', 'street art', 'documentary', 'art deco', 'photorealism', 'folk', 'cubism', 'dada' ]
 end
