f954424b75cb58d1c3fd3e47c780233b1908fa95
Rohit Sharma <rohitsharma@MacBook-Pro-2.broadband>
2021-09-20 12:41:34 +0100

    first

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,12 +3,10 @@ class ContactsController < ApplicationController
 
     def new
       @contact = Contact.new
-      # authorize @contact
     end
   
     def create
       @contact = Contact.new(params[:contact])
-      # authorize @contact
       @contact.request = request
       if @contact.deliver
         flash.now[:error] = 'Message sent!'
