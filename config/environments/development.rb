ec9703334b547f9c70d607a47dc8ff4c3fca552d
Rohit Sharma <rohitsharma@MacBook-Pro-2.broadband>
2021-09-21 13:23:06 +0100

    first

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,7 +72,7 @@ config.active_storage.service = :cloudinary
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors= true
   config.action_mailer.delivery_method = :smtp
