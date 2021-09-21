065c3281d9a540fda8f2510b6e99d94b8b2451bb
Rohit Sharma <rohitsharma@MacBook-Pro-2.broadband>
2021-09-21 13:25:48 +0100

    f

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -138,13 +138,13 @@ Rails.application.configure do
 config.action_mailer.default_url_options = { :host => 'https://arts4all.herokuapp.com/' }  
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = false
+config.action_mailer.raise_delivery_errors = true
 config.action_mailer.smtp_settings = {
-:user_name => ENV['SENDGRID_USERNAME'],
-:password => ENV['SENDGRID_PASSWORD'],
-:domain => 'heroku.com',
 :address => 'smtp.sendgrid.net',
 :port => 587,
+:domain => 'heroku.com',
+:user_name => ENV['SENDGRID_USERNAME'],
+:password => ENV['SENDGRID_PASSWORD'],
 :authentication => :plain,
 :enable_starttls_auto => true
 }
