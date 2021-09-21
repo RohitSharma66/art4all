de46e81786b4c514fecc8a7394eec1026b1a9c29
Rohit Sharma <rohitsharma@MacBook-Pro-2.broadband>
2021-09-21 13:11:04 +0100

    first

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -139,7 +139,7 @@ config.action_mailer.default_url_options = { :host => 'https://arts4all.herokuap
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = false
-ActionMailer::Base.smtp_settings = {
+config.action_mailer.smtp_settings = {
 :user_name => ENV['SENDGRID_USERNAME'],
 :password => ENV['SENDGRID_PASSWORD'],
 :domain => 'heroku.com',
