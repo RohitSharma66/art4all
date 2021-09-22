bfc603c1c6eddcf789f5f0acf0586f59574748c3
Rohit Sharma <rohitsharma@MacBook-Pro-2.broadband>
2021-09-22 12:46:47 +0100

    f

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,6 @@
 class ContactsController < ApplicationController
   after_action :verify_authorized, except: [:new, :create], unless: :skip_pundit?
-
+  before_action :contact_params, only: [:create]
     def new
       @contact = Contact.new
     end
@@ -9,10 +9,15 @@ class ContactsController < ApplicationController
       @contact = Contact.new(params[:contact])
       @contact.request = request
       if @contact.deliver
-        flash.now[:error] = 'Message sent!'
+        flash.now[:notice] = 'Thank you for your message!'
       else
         flash.now[:error] = 'Could not send message'
         render :new
       end
+    end
+
+    private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :message, :nickname, :captcha)
     end
 end
