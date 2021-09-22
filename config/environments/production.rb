710108733077378582e9de9bfc4da2d1f904d50b
Rohit Sharma <rohitsharma@MacBook-Pro-2.broadband>
2021-09-22 13:26:53 +0100

    first

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -139,13 +139,13 @@ config.action_mailer.default_url_options = { :host => 'https://arts4all.herokuap
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
 config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-address:              'smtp.gmail.com',
-port:                 587,
-domain:               'https://arts4all.herokuapp.com/',
-user_name:            ENV['SENDGRID_USERNAME'],
-password:             ENV['SENDGRID_USERNAME'],
-authentication:       'plain',
-enable_starttls_auto: true 
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'https://arts4all.herokuapp.com/',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
 }
 end
